5a0825b89a0b0183bf8469e66b667e294309b609
Daniel Morlock <morlock@videmo.de>
2016-01-13 14:49:43 +0100

    Update SabreDAV from 1.8.6 to 3.1.0

--- plugins/calendar/lib/SabreDAV/vendor/sabre/vobject/tests/bootstrap.php
+++ plugins/calendar/lib/SabreDAV/vendor/sabre/vobject/tests/bootstrap.php
@@ -1,13 +1,25 @@
 <?php
 
-$try = array(
+date_default_timezone_set('UTC');
+
+$try = [
     __DIR__ . '/../vendor/autoload.php',
     __DIR__ . '/../../../autoload.php',
-);
+];
 
-foreach($try as $path) {
+foreach ($try as $path) {
     if (file_exists($path)) {
-        include $path;
+        $autoLoader = include $path;
         break;
     }
+}
+
+$autoLoader->addPsr4('Sabre\\VObject\\', __DIR__ . '/VObject');
+
+if (!defined('SABRE_TEMPDIR')) {
+  define('SABRE_TEMPDIR', __DIR__ . '/temp/');
+}
+
+if (!file_exists(SABRE_TEMPDIR)) {
+  mkdir(SABRE_TEMPDIR);
 }
